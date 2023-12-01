627b5cc56011449a7e78edb34ee64fab9f30c0cd
zw2821 <zw2821@skbl-418-007.ph.ic.ac.uk>
2023-12-01 12:13:30 +0000

    dec01/temp_sensor

--- main.s
+++ main.s
@@ -1,23 +1,23 @@
 #include <xc.inc>
 extrn	UART_Setup, UART_Transmit_Message  ; external uart subroutines
-extrn	LCD_Setup, LCD_Write_Message, LCD_Write_Hex, LCD_Send_Byte_D ; external LCD subroutines
-extrn	ADC_Setup, ADC_Read, ADC_Mul_k, ADC_Mul_10, ADC_Output		   ; external ADC subroutines
+extrn	LCD_Setup, LCD_Write_Message, LCD_Write_Hex, LCD_Send_Byte_D, LCD_line2 ; external LCD subroutines
+extrn	ADC_Setup,ADC_Output, Thermal_sensor_read		   ; external ADC subroutines
 
 psect	udata_acs   ; reserve data space in access ram
 counter:    ds 1    ; reserve one byte for a counter variable
 delay_count:ds 1    ; reserve one byte for counter in the delay routine
-ADC_output_array:   ds 4
+
     
-;psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
-;myArray:    ds 0x80 ; reserve 128 bytes for message data
+psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
+Temp_array:    ds 0x80 ; reserve 128 bytes for message data
 ;
-;psect	data    
+psect	data    
 ;	; ******* myTable, data in programme memory, and its length *****
-;myTable:
-;	db	'H','e','l','l','o',' ','W','o','r','l','d','!',0x0a
+Temp_display:
+	db	'T','E','M','P',':',0x0a
 ;					; message, plus carriage return
-;	myTable_l   EQU	13	; length of data
-;	align	2
+	length   EQU	6	; length of data
+	align	2
     
 psect	code, abs	
 rst: 	org 0x0
@@ -26,50 +26,42 @@ rst: 	org 0x0
 	; ******* Programme FLASH read Setup Code ***********************
 setup:	bcf	CFGS	; point to Flash program memory  
 	bsf	EEPGD 	; access Flash program memory
-	call	UART_Setup	; setup UART
+	;call	UART_Setup	; setup UART
 	call	LCD_Setup	; setup UART
 	call	ADC_Setup	; setup ADC
-	goto	measure_loop
+	goto	start
 	
 	; ******* Main programme ****************************************
-;start: 	lfsr	0, myArray	; Load FSR0 with address in RAM	
-;	movlw	low highword(myTable)	; address of data in PM
-;	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
-;	movlw	high(myTable)	; address of data in PM
-;	movwf	TBLPTRH, A		; load high byte to TBLPTRH
-;	movlw	low(myTable)	; address of data in PM
-;	movwf	TBLPTRL, A		; load low byte to TBLPTRL
-;	movlw	myTable_l	; bytes to read
-;	movwf 	counter, A		; our counter register
-;loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-;	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-;	decfsz	counter, A		; count down to zero
-;	bra	loop		; keep going until finished
-;		
-;	movlw	myTable_l	; output message to UART
-;	lfsr	2, myArray
-;	call	UART_Transmit_Message
-;
-;	movlw	myTable_l-1	; output message to LCD
-;				; don't send the final carriage return to LCD
-;	lfsr	2, myArray
-;	call	LCD_Write_Message
+start: 	lfsr	0, Temp_array	; Load FSR0 with address in RAM	
+	movlw	low highword(Temp_display)	; address of data in PM
+	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
+	movlw	high(Temp_display)	; address of data in PM
+	movwf	TBLPTRH, A		; load high byte to TBLPTRH
+	movlw	low(Temp_display)	; address of data in PM
+	movwf	TBLPTRL, A		; load low byte to TBLPTRL
+	movlw	length	; bytes to read
+	movwf 	counter, A		; our counter register
+loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
+	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
+	decfsz	counter, A		; count down to zero
+	bra	loop		; keep going until finished
+		
+	movlw	length	; output message to UART
+	;lfsr	2, myArray
+	;call	UART_Transmit_Message
+
+	movlw	length-1	; output message to LCD
+				; don't send the final carriage return to LCD
+	lfsr	2, Temp_display
+	call	LCD_Write_Message
+	call	LCD_line2
 	
 measure_loop:
-   
-	lfsr	1, ADC_output_array
-	call	ADC_Read
+	call	Thermal_sensor_read
 	;movf	ADRESH, W, A
 	;call	LCD_Write_Hex
 	;movf	ADRESL, W, A
 	;call	LCD_Write_Hex
-	
-	
-	call	ADC_Mul_k
-	call	ADC_Mul_10
-	call	ADC_Mul_10
-	call	ADC_Mul_10
-	lfsr	2, ADC_output_array
 	call	ADC_Output
 	goto	measure_loop	; goto current line in code
 	
