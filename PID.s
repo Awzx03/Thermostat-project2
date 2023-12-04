6dab5b5796aa1960b9349b8de058428899f57e19
zw2821 <zw2821@skbl-418-007.ph.ic.ac.uk>
2023-12-04 16:53:11 +0000

    dec04PID

--- PID.s
+++ PID.s
@@ -1,21 +1,52 @@
 #include <xc.inc>
     
-global  PID_error
+global  PID_error, PID_Setup
 extrn	ADC_Setup, ADC_Output, Thermal_sensor_read, ADC_output_array
     
 psect	udata_acs   ; reserve data space in access ram
-Kp:	    equ 0.1
-Ki:	    equ 0.01
-Kd:	    equ 0.005
+Kp	    equ 1
+;Ki:	    equ 0.01
+;Kd:	    equ 0.005
 	    	
 set_T:	    ds 4
-current_T:  ds 4-
-error:	    ds 10	    ; reserve 1 byte for variable UART_counter
-Output:	    equ 0x40
+current_T:  ds 4
+error_T:    ds 4
+Output	    equ 0x40
     
 psect	uart_code,class=CODE
-
+PID_Setup:
+    movlw   0x00
+    movwf   set_T
+    movwf   set_T+3
+    movlw   0x02
+    movwf   set_T+1
+    movlw   0x08
+    movwf   set_T+2
+    return
 PID_error:
+    movff   ADC_output_array, current_T, A
+    movff   ADC_output_array+1, current_T+1, A
+    movff   ADC_output_array+2, current_T+2, A
+    movff   ADC_output_array+3, current_T+3, A
+
+    movf    current_T+3, W, A
+    subwf   set_T+3, W, A
+    movwf   error_T+3, A
+
+    
+    movf    current_T+2, W, A
+    subwfb  set_T+2, W, A
+    movwf   error_T+2, A
+    
+    movf    current_T+1, W, A
+    subwfb  set_T+1, W, A
+    movwf   error_T+1, A
+    
+    movf    current_T, W, A
+    subwfb  set_T, W, A
+    movwf   error_T, A
+    
+    return
     
     
     
