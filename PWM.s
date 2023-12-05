de9409ef4bd2cebc873053030ae4257023d97231
zw2821 <zw2821@skbl-418-007.ph.ic.ac.uk>
2023-12-05 12:09:59 +0000

    dec05PWM

---
+++ PWM.s
@@ -0,0 +1,33 @@
+#include <xc.inc>
+    
+global  PWM_Setup, PWM_update
+extrn	ADC_Setup, ADC_Output, Thermal_sensor_read, ADC_output_array
+    
+psect	udata_acs   ; reserve data space in access ram
+duty_cycle	    equ 0x40
+    
+psect	uart_code,class=CODE
+PWM_Setup:
+    bcf	    TRISC, PORTC_RC2_POSN, A 
+    bcf	    TRISG, PORTG_RG3_POSN, A 
+    movlw   0x3C
+    movwf   CCP4CON, A
+    movlw   0xF9
+    movwf   CCPR4L, A
+    movlw   0x00
+    movwf   CCPR4H, A	;set duty cycle 100%
+    movlw   0x06
+    movwf   T2CON, A
+    movlw   0xF9
+    movwf   PR2		;period 0.25ms
+    clrf    CCPTMRS1
+    return
+    
+    
+PWM_update:
+    movlw   0xF9
+    movwf   CCPR4L, A
+    return
+
+
+end
