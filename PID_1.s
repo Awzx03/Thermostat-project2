7052aa270edfe7727cfcdd15217ac89529965541
zw2821 <zw2821@skbl-418-007.ph.ic.ac.uk>
2023-12-07 12:26:34 +0000

    07Dec/PID

--- PID_1.s
+++ PID_1.s
@@ -1,6 +1,6 @@
 #include <xc.inc>
     
-global  PID_error, PID_Setup, dec_convert
+global  PID_error, PID_Setup, dec_convert, PWM_output, PID_output
 extrn	ADC_Setup, ADC_Output, Thermal_sensor_read, ADC_output_array
     
 psect	udata_acs   ; reserve data space in access ram
@@ -8,22 +8,21 @@ Kp	    equ 1
 ;Ki:	    equ 0.01
 ;Kd:	    equ 0.005
 	    	
-set_T:	    ds 2
-current_T_low: ds 1
-current_T_high: ds 1
-former_T_low:  ds 1
-former_T_high: ds 1
-error_T_low:    ds 1
-error_T_high:    ds 1
-error_sum_low:  ds 1
-error_sum_high:  ds 1
-error_d_low:	ds 1
-error_d_high:	ds 2
-time_count:	ds 2
-
-set_T_low:  ds 1
-set_T_high: ds 1
-Output	    equ 0x40
+set_T:			ds 2
+current_T_low:		ds 1
+current_T_high:		ds 1
+former_e_low:		ds 1
+former_e_high:		ds 1
+error_T_low:		ds 1
+error_T_high:		ds 1
+error_sum_low:		ds 1
+error_sum_high:		ds 1
+error_d_low:		ds 1
+error_d_high:		ds 2
+time_count:		ds 2
+set_T_low:		ds 1
+set_T_high:		ds 1
+PWM_output:		ds 1
 
 	    
 psect	uart_code,class=CODE
@@ -33,6 +32,8 @@ PID_Setup:
     clrf    set_T_low, A
     clrf    set_T_high, A
     clrf    time_count, A
+    clrf    former_e_low
+    clrf    former_e_high
     call    dec_convert
     return
     
@@ -40,10 +41,8 @@ PID_Setup:
 PID_error:
     movlw   0x01
     addwf   time_count
-    movlw   0xD2
-    movwf   current_T_low, A
-    movlw   0x00
-    movwf   current_T_high, A
+    movff   ADRESL, current_T_low, A
+    movff   ADRESH, current_T_high, A
     
     movf    current_T_low, W, A
     subwf   set_T_low, W, A
@@ -52,25 +51,25 @@ PID_error:
     subwfb  set_T_high, W, A
     movwf   error_T_high
     
-    ;movff   ADRESL, current_T_low, A
-    ;movff   ADRESH, current_T_high, A
-    
+
 PID_integral:
     movf    error_T_low, W, A
     addwf   error_sum_low, f, A
-    movf    erro_T_high, W, A
+    movf    error_T_high, W, A
     addwfc  error_sum_high, f, A
 
 PID_derivative: 
-    movf    former_T_low, W, A
-    subwf   current_T_low, W, A
+    
+    movf    former_e_low, W, A
+    subwf   error_T_low, W, A
     movwf   error_d_low
-    movf    former_T_high, W, A
-    subwfb  current_T_high, W, A
+    movf    former_e_high, W, A
+    subwfb  error_T_high, W, A
     movwf   error_d_high
     
-    movff   current_T_low, former_T_low, A
-    movff   current_T_high, former_T_high, A
+    movff   error_T_low, former_e_low, A
+    movff   error_T_high, former_e_high, A
+    return
     
     
   
@@ -90,9 +89,9 @@ PID_derivative:
 ;     return
     
 dec_convert:
-    movlw   0x03
+    movlw   0x02
     movwf   0x40, A
-    movlw   0x05
+    movlw   0x08
     movwf   0x41, A
    
     
@@ -130,8 +129,22 @@ add_100:
     
 
 
-
-
+PID_output:
+   ; movlw   0xf
+    ;cpfsgt  
+    
+    movlw   0x64
+    cpfsgt  error_T_low, A
+    goto    P_control
+    movlw   0xf9
+    movwf   PWM_output
+    return
+    
+P_control:
+    movlw   0x02
+    mulwf   error_T_low, A
+    movff   PRODL, PWM_output
+    return
     
 
 
